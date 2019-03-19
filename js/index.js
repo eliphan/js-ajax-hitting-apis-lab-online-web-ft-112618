23edd0687acf1d34b7dc3a44ee05e9d9fe33a1c1
Elise Phan <aliceconnic@gmail.com>
2019-03-19 19:21:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -24,3 +24,28 @@ function displayCommits() {
 
 
 
+function getCommits(el) {
+  const repoName = el.dataset.repository;
+  const uri =
+    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/commits';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayCommits);
+  xhr.open('GET', uri);
+  xhr.send();
+}
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><h3>' +
+        commit.commit.author.name +
+        ' (' +
+        commit.author.login +
+        ')</h3>' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
