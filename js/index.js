33ede4c95417dc9d86a132338365bc4f2aeaf270
Elise Phan <aliceconnic@gmail.com>
2019-03-19 19:11:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // your code here
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
